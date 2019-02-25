92ace890282b325477e497b89ed88daac13fa2be
Glitch (cool-parakeet) <none>
2019-02-25 09:49:38 +0000

    🎳📕 Checkpoint ./routes/exercise.js:3244735/1160 ./server.js:3244735/3

--- routes/exercise.js
+++ routes/exercise.js
@@ -4,26 +4,31 @@ const express = require('express'),
       { User } = require('./../models/User'),
       { Exercise } = require('./../models/Exercise');
 
-const middleware = (req, res, next) => {
-  const user = new User({ username: req.body.username });
-  const doUserExist = User.findOne({ username: user.username }).then((userID) => {
-    if(userID.username === user.username) {
+// const middleware = (req, res, next) => {
+//   const user = new User({ username: req.body.username });
+//   const doUserExist = User.findOne({ username: user.username }).then((userID) => {
+//     if(userID.username === user.username) {
     
-    }
-      return res.json({ error: "Username has already been taken!" });
-    return user;
-  });
-};
+//     }
+//       return res.json({ error: "Username has already been taken!" });
+//     return user;
+//   });
+// };
 
 router.use(bodyParser.urlencoded({extended: false}));
 router.use(bodyParser.json());
 
-router.post('/new-user', (req, res) => {
-
-});
-
 router.get('/users', (req, res) => {
   User.find().then((users) => res.json(users), (e) => res.status(400).json({ error: e }));
+});
+
+router.post('/new-user', (req, res) => {
+  const user = new User({ username: req.body.username });
+  User.findOne({ username: user.username }).then((userID) => {
+      if(userID.username === user.username) {
+        res.json({ error: "Username has already been taken!" });
+      }
+  });
 });
 
 router.post('/add', (req, res) => res.send('add exercise'));
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
-app.use('/api/exercise', require('./routes/exercise.js'));
+app.use('/api/exercise', require('./routes/exercise'));
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
