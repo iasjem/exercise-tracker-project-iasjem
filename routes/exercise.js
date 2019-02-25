41225f99b4634de5f8598754b4ef9b3c9e1bf5a2
Glitch (cool-parakeet) <none>
2019-02-25 08:46:03 +0000

    🏗🦂 Checkpoint ./routes/exercise.js:3244735/1251

--- routes/exercise.js
+++ routes/exercise.js
@@ -9,8 +9,14 @@ router.use(bodyParser.json());
 
 router.post('/new-user', (req, res) => {
   const user = new User({ username: req.body.username });
-  res.send(user);
+  User.find()
+  user.save().then(() => res.status(200).json(user), (e) => res.status(400).json({ error: e }));
 });
+
+router.get('/users', (req, res) => {
+  User.find().then((users) => res.json(users), (e) => res.status(400).json({ error: e }));
+});
+
 router.post('/add', (req, res) => res.send('add exercise'));
 
 module.exports = router;
