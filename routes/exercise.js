1e6c1a22e165598b97151b05e31f1a325f2ad463
Glitch (cool-parakeet) <none>
2019-02-25 09:08:03 +0000

    🍘👂 Checkpoint ./routes/exercise.js:3244735/1373

--- routes/exercise.js
+++ routes/exercise.js
@@ -4,17 +4,22 @@ const express = require('express'),
       { User } = require('./../models/User'),
       { Exercise } = require('./../models/Exercise');
 
+const middleware = (req, res, next) => {
+  const user = new User({ username: req.body.username });
+  const doUserExist = User.findOne({ username: user.username }).then((userID) => {
+    if(userID.username === user.username) {
+    
+    }
+      return res.json({ error: "Username has already been taken!" });
+    return user;
+  });
+};
+
 router.use(bodyParser.urlencoded({extended: false}));
 router.use(bodyParser.json());
 
 router.post('/new-user', (req, res) => {
-  const user = new User({ username: req.body.username });
-  const doUserExist = User.findOne({ username: user.username }).then((userID) => {
-    if(userID.username === user.username) return res.json({ error: "Username has already been taken!" });
-    user.save().then(() => res.status(200).json(user), (e) => res.status(400).json({ error: e }));
-  });
-  
-  
+
 });
 
 router.get('/users', (req, res) => {
