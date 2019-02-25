aa22e6ebf230c877531ebf49f1da50917af49e56
Glitch (cool-parakeet) <none>
2019-02-25 08:14:03 +0000

    👩🦃 Checkpoint ./server.js:3244735/411 ./routes/index.js:3244735/3

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+app.use(require('./controllers'));
 
 
 // // Not found middleware
